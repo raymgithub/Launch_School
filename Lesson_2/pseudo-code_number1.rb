6b7f72d7ef14896f156456fdd35f53c13d474110
Raymond <ichigoray@gmail.com>
2023-09-23 19:40:49 -0400

    Add informal code, Add RESULT to formal code

--- Lesson_2/pseudo-code_number1.rb
+++ Lesson_2/pseudo-code_number1.rb
@@ -5,6 +5,16 @@ START
 GET num1 = first number from user
 GET num2 = second number from user
 
-PRINT num1 + num2 
 
+
+SET RESULT = num1 + num2 
+
+PRINT RESULT
 END
+
+
+GET first number
+GET second number
+ADD first number to second number
+SET line ADD first number to second number to RESULT
+print RESULT
