127faf1cb70477e419d12eaf94926a9853d0f725
Raymond <ichigoray@gmail.com>
2023-09-23 23:48:00 -0400

    Create pseudo-code_5

---
+++ Lesson_2/pseudo-code_number5.rb
@@ -0,0 +1,1 @@
+#a method that takes two arrays of numbers and returns the result of merging the arrays. The elements of the first array should become the elements at the even indexes of the returned array, while the elements of the second array should become the elements at the odd indexes. For instance: 
