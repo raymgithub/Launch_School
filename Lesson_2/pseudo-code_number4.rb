f101fba84ba80556559368dc220e80792c92f97d
Raymond <ichigoray@gmail.com>
2023-09-23 23:46:12 -0400

    Add Code

--- Lesson_2/pseudo-code_number4.rb
+++ Lesson_2/pseudo-code_number4.rb
@@ -9,3 +9,23 @@ If any given character is not repeated more than 3 times
   -return nil
 PRINT the index of the 3rd character
   
+
+START
+
+GET string 
+Iterate through string and index each letter to an array
+  -Start a count for each charactar starting at 0
+    if charactar is repeated add to the count
+    if charactar is repeated more than 3 times
+        return the index of the 3rd occurrence of the character
+    if charactar any given charactar is not repeated 3 times 
+        return nil
+    end
+
+else 
+
+
+
+
+
+END
