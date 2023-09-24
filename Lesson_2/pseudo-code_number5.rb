f29aad08bb9fa819365a09a5105f6a0d9a32030e
Raymond <ichigoray@gmail.com>
2023-09-24 00:08:53 -0400

    Add informal code

--- Lesson_2/pseudo-code_number5.rb
+++ Lesson_2/pseudo-code_number5.rb
@@ -1,1 +1,6 @@
 #a method that takes two arrays of numbers and returns the result of merging the arrays. The elements of the first array should become the elements at the even indexes of the returned array, while the elements of the second array should become the elements at the odd indexes. For instance: 
+
+GET first array numbers
+GET second array of numbers
+Merge first array with second array
+Second Array should be added to first array at the odd index without removing any elements in the first array
