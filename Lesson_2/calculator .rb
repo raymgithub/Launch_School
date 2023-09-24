129d224792cdaac09c7b8289b369ea36df0d49da
Raymond <ichigoray@gmail.com>
2023-09-24 17:40:13 -0400

    Add case statement

--- Lesson_2/calculator .rb
+++ Lesson_2/calculator .rb
@@ -11,20 +11,20 @@ loop do
     n2 = gets.chomp
     prompt("What do you want to do? 1.)Add 2.)Subtract 3.)Multiply 4.)Divide")
     action = gets.chomp.to_s.upcase
-
-    if action == "ADD" ||  action == "1"
-      result = n1.to_i + n2.to_i
-    elsif action == "SUBTRACT" || action == "2"
-      result = n1.to_i - n2.to_i  
-    elsif action == "SUBTRACT" || action == "3"
-      result = n1.to_i * n2.to_i  
-    elsif action == "DIVIDE" || action == "4"
-      result = n1.to_f / n2.to_f  
-    else 
+     
+    result = case action
+    when '1'
+      n1.to_i + n2.to_i
+    when '2'
+      n1.to_i - n2.to_i
+    when '3'
+      n1.to_i * n2.to_i
+    when '4'
+      n1.to_f / n2.to_f
+    else
       prompt("Invalid action! Try again!")
       break
     end
-      
     prompt("The result is #{result}")
     return
   end
