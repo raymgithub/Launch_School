d4e5a499710581b64d2cb2007c0839c9bc11f6b9
Raymond <ichigoray@gmail.com>
2023-09-23 20:08:23 -0400

    Added text and code

---
+++ Lesson_2/pseudo-code_number2.rb
@@ -0,0 +1,15 @@
+#a method that takes an array of strings, and returns a string that is all those strings concatenated together 
+
+GET array of strings
+CONCANTENATE string using kernal.concat()
+PRINT STRINGS
+
+START
+GET array_of_string
+
+array_of_string.concat
+
+array_of_string
+END
+
+puts array_of_string
