7c045c2ce82f3131d31172b5611fb864ca85a084
Raymond <ichigoray@gmail.com>
2023-09-25 14:22:56 -0400

    Add pseudo code

---
+++ Lesson_2/Car_Loan_Calculator.rb
@@ -0,0 +1,11 @@
+Find the total amount of the loan
+  -Get the APR percertage that was locked in
+  -Get the loan duration
+    -Find the APR total base on the loan duration
+      -Find the total per month payment for the duration that the loan is active for
+
+
+
+
+
+  
