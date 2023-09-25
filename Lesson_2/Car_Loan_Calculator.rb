7fc2bf5ad4da8283d9b844bfb119183dcf0f06a6
Raymond <ichigoray@gmail.com>
2023-09-25 15:49:50 -0400

    Add to pseudo-code

--- Lesson_2/Car_Loan_Calculator.rb
+++ Lesson_2/Car_Loan_Calculator.rb
@@ -1,30 +1,9 @@
+=begin
 Find the total amount of the loan
   -Get the APR percertage that was locked in
   -Get the loan duration
     -Find the APR total base on the loan duration
-      -Find the total per month payment for the duration that the loan is active for
-
-def total_amount(amount)
-
-end
-
-def total_apr(apr)
-
-end
-
-def total_duration(duration)
-
-end
-
-def total_payment(payment)
-
-end
-
-
-
-puts "Please enter the total amount of the loan"
-total_amount = gets.chomp
-puts "Please enter the total duration of the loan"
-total_duration = gets.chomp
-puts "Please enter the APR that was locked in."
-total_apr = gets.chomp  
+      -Divide the total APR by the loan duration to get monthly APR payment
+        -Find the total per month payment for the duration that the loan is active for
+          -Add the monthly APR payment to the total per month  
+=end
