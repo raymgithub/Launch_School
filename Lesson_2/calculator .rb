6e8847180c5b2ec169c67c8fcf37505de0765d19
Raymond <ichigoray@gmail.com>
2023-09-25 13:55:06 -0400

    Change valid_number? method

--- Lesson_2/calculator .rb
+++ Lesson_2/calculator .rb
@@ -3,7 +3,7 @@ def prompt(message)
 end
 
 def valid_number?(num)
- num.to_i() != 0
+  num.to_i.to_s == num
 end
 
 def action_to_message(op)
@@ -19,29 +19,31 @@ def action_to_message(op)
   end
 end
 
+=begin
 prompt("Welcome to Calculator! Enter your name:")
 
 name = ''
 loop do
- name = Kernel.gets().chomp()
+  name = Kernel.gets().chomp()
 
  if name.empty?()
-  prompt("Make sure to use a valid name.")
- else 
-  break
+   prompt("Make sure to use a valid name.")
+ else
+   break
  end
 end
 
 prompt("Hello #{name}")
+=end 
 
 loop do
 
-  n1 = ''
+    n1 = ''
 
   loop do
-    prompt("Enter your first number, #{name}")
+    prompt("Enter your first number")
     n1 = gets.chomp
-      
+    
     if valid_number?(n1)
       break
     else
@@ -51,17 +53,17 @@ loop do
 
   n2 = ''
 
-  loop do 
+  loop do
     prompt("Enter your second number")
     n2 = gets.chomp
-    
+  
     if valid_number?(n2)
       break
     else
       prompt("Invalid number!")
     end
   end
-  
+
   action_prompt = <<-MSG
     What operation would you like to perform?
     1) Add
@@ -74,34 +76,33 @@ loop do
 
   action = ' '
   loop do
-      action = gets.chomp
-  
+    action = gets.chomp
+
       if %w(1 2 3 4).include?(action)
         break
       else
         prompt("Must choose 1, 2, 3 or 4")
-    end
+      end
   end
 
-    prompt("#{action_to_message(action)} the numbers #{n1} and #{n2}")
+  prompt("#{action_to_message(action)} the numbers #{n1} and #{n2}")
 
 
     result = case action
-    when '1'
-      n1.to_i + n2.to_i
-    when '2'
-      n1.to_i - n2.to_i
-    when '3'
-      n1.to_i * n2.to_i
-    when '4'
-      n1.to_f / n2.to_f
-    end
-      
-      
-    prompt("The result is #{result}")
-      
-    prompt("Do you want to perform another calculation? (Y to calculate again)")
-    answer = Kernel.gets().chomp()
-    break unless answer.downcase().start_with?('y')
+              when '1'
+                n1.to_i + n2.to_i
+              when '2'
+                n1.to_i - n2.to_i
+              when '3'
+                n1.to_i * n2.to_i
+              when '4'
+                n1.to_f / n2.to_f
+              end
+
+  prompt("The result is #{result}")
+
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
   end
-prompt("Thank you for using the calculator #{name}!")
+#prompt("Thank you for using the calculator #{name}!")
