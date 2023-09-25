b3f2d87f1f95b34e4c9b900564475b464d2a5204
Raymond <ichigoray@gmail.com>
2023-09-25 15:50:31 -0400

    Add monthly_payment method

--- Lesson_2/Car_Loan_Calculator.rb
+++ Lesson_2/Car_Loan_Calculator.rb
@@ -7,3 +7,22 @@ Find the total amount of the loan
         -Find the total per month payment for the duration that the loan is active for
           -Add the monthly APR payment to the total per month  
 =end
+total_amount = ''
+total_apr = ''
+total_duration = ''
+
+
+def monthly_payment(amount, duration)
+  amount.to_f / duration.to_i
+end
+
+
+puts "Please enter the total amount of the loan"
+total_amount = gets.chomp
+puts "Please enter the total duration of the loan in months"
+total_duration = gets.chomp
+puts "Please enter the APR that was locked in."
+total_apr = gets.chomp  
+
+puts monthly_payment(total_amount, total_duration)
+
