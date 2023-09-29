e5f777516cfcf1c9b1257bcb5f440bec94d52c48
Raymond <ichigoray@gmail.com>
2023-09-28 23:18:02 -0400

    Add Psuedo-code

---
+++ Lesson_2/Rock-Paper-Scissor.rb
@@ -0,0 +1,16 @@
+Provide three choices for user
+  -Rock, Paper, Scissor
+    -Rock will win against Scissor
+      -lose to Paper
+    -Paper should be valued over Rock
+      -lose to Scissor
+    -Scissor should be valued Paper
+      -lose to Rock
+Ask user for a choice
+  -Assign computer with a random choice
+    -Compare the result
+      -Print result
+        -Ask user if they want to play again
+          -If yes, play again
+          -If no, exit the program
+          
